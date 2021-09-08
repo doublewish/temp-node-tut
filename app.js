d9f0121be7380fe522fab1bbb88cc09504ce318f
Brett Grentell <doublewish@spickatron.com>
2021-09-08 14:39:49 +1000

    Complete source code

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
 #!/usr/bin/env node
-const util = require('util')
-const timerPromise = util.promisify(setTimeout)
-
-
-
-const start = async() => {
-  try {
-    const res = await timerPromise(1000, "Finally")
-    console.log(res)
-  } catch (err) {
-    console.log(err)
-  }
-}
-
-start()
